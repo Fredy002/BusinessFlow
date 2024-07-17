d97bc31774c0bbcdc10f9583e309c17405ae5df3
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-16 21:05:11 -0500

    Refactor CardSummary component and update average revenue display

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,6 +1,8 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 import { CustomTooltip } from "@/components/CustomTooltip";
+import { cn } from "@/lib/utils";
+import { MoveDownRight, MoveUpRight, TrendingUp } from "lucide-react";
 
 export function CardSummary(props: CardSummaryProps) {
 
@@ -14,6 +16,22 @@ export function CardSummary(props: CardSummaryProps) {
                     {title}
                 </div>
                 <CustomTooltip content={tooltipText} />
+            </div>
+            <div className="flex gap-4 mt-2 md:mt-4">
+                <p className="text-2xl">
+                    {total}
+                </p>
+                <div className={cn(`flex items-center gap-1 px-2 text-xs text-white rounded-lg h-[20px] bg-black dark:bg-secondary`)}>
+                    {average}%
+
+                    {average < 20 ? (
+                        <MoveDownRight strokeWidth={2} className="h-4 w-4" />
+                    ) : average >= 20 && average < 70 ? (
+                        <MoveUpRight strokeWidth={2} className="h-4 w-4" />
+                    ) : (
+                        <TrendingUp strokeWidth={2} className="h-4 w-4" />
+                    )}
+                </div>
             </div>
         </div>
     )
