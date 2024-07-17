8abb3351729015b4edccd2aa9658ab715cd67c84
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-17 15:24:20 -0500

    refactor: Update layout.tsx to use ThemeProvider for consistent theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Noto_Sans_Display } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css";
-import {ClerkProvider} from '@clerk/nextjs'
 
 const noto = Noto_Sans_Display({ subsets: ["latin"] });
 
@@ -19,7 +21,14 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={noto.className}>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
