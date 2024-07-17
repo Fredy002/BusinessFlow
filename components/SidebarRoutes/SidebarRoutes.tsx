c99d0ccba0d0e96c4c9b15a5a5048c8cf6ca17a0
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-17 00:12:11 -0500

    refactor: Update SidebarRoutes component to include General, Tools, and Support sections with respective data and UI elements

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,7 +1,64 @@
+"use client";
+
+import { GeneralSidebarData, SupportSidebarData, ToolsSidebarData } from "@/lib/constants/SidebarRoutes.data";
+import { SidebarItem } from "../SidebarItem";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+
 export function SidebarRoutes() {
     return (
-        <div>
-            SidebarRoutes
+        <div className="flex flex-col justify-between h-full">
+            <div>
+                <div className="p-2 md:p-6">
+                    <p>
+                        GENERAL
+                    </p>
+                    {GeneralSidebarData.map((item, index) => (
+                        <SidebarItem key={index.toString()} item={item} />
+                    ))}
+                </div>
+
+                <Separator />
+                <div className="p-2 md:p-6">
+                    <p>
+                        TOOLS
+                    </p>
+                    {ToolsSidebarData.map((item, index) => (
+                        <SidebarItem key={index.toString()} item={item} />
+                    ))}
+                </div>
+
+                <Separator />
+                <div className="p-2 md:p-6">
+                    <p>
+                        TOOLS
+                    </p>
+                    {SupportSidebarData.map((item, index) => (
+                        <SidebarItem key={index.toString()} item={item} />
+                    ))}
+                </div>
+            </div>
+
+            <div>
+                <div className="text-center p-6">
+                    <Button variant="outline" className="w-full">
+                        Upgrade Plan
+                    </Button>
+                </div>
+
+                <Separator />
+                <footer className="text-center p-3 mt-3">
+                    Debeloped by{" "}
+                    <a
+                        href="https://github.com/fredy002"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline"
+                    >
+                        Fredy
+                    </a>
+                </footer>
+            </div>
         </div>
     )
 }
