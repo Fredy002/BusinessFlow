3092007edbd8cb0aae08429d94637be785296720
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-16 21:53:00 -0500

    feat: Add lazy loading for images in Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,41 @@
+import { Input } from "@/components/ui/input"
 
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
-export  function Navbar() {
+import { UserButton } from "@clerk/nextjs"
+import { Menu, Search } from "lucide-react"
+
+export function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
+
+            {/* Mobile */}
+            <div className="block md:hidden">
+                <Sheet>
+                    <SheetTrigger className="flex items-center">
+                        <Menu />
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <p>
+                            Sidebar Routes
+                        </p>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
+            <div className="relative w-[300px]">
+                <Input placeholder="Search" className="rounded-lg" />
+                <Search className="absolute top-2 right-2" strokeWidth={1} />
+            </div>
+
+            <div className="flex gap-x-2 items-center">
+                <p>Toogle Theme</p>
+                <UserButton />
+            </div>
         </div>
     )
 }
