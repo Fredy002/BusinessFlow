163fe0555290259161174de21f64f97dfd7ca2d2
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-16 20:01:43 -0500

    Refactor CardSummary component and add CustomIcon and CustomTooltip components

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,30 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import { CardSummary } from "./components/CardSummary";
-import { BookOpenCheck, KeyboardMusic, Waypoints } from "lucide-react";
-
-export const dataCardsSummary = [
-  {
-    icon: KeyboardMusic,
-    total: "100",
-    average: 5,
-    title: "Products Created",
-    tooltipText: "See all products created",
-  },
-  {
-    icon: Waypoints,
-    total: "86.5%",
-    average: 80,
-    title: "Total Revenue",
-    tooltipText: "See all of the revenue",
-  },
-  {
-    icon: BookOpenCheck,
-    total: "S/. 265",
-    average: 80,
-    title: "Bounce Rate",
-    tooltipText: "See all of the bounce rate",
-  },
-];
+import { dataCardsSummary } from "@/lib/constants/dataCardsSummary";
 
 export default function Home() {
   return (
