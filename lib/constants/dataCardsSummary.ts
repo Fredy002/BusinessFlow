8cff9e366db0cd496e192afa96a5f7f1aba76b65
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-16 20:01:11 -0500

    feat: Add dataCardsSummary constant

---
+++ lib/constants/dataCardsSummary.ts
@@ -0,0 +1,25 @@
+import { BookOpenCheck, KeyboardMusic, Waypoints } from "lucide-react";
+
+export const dataCardsSummary = [
+    {
+        icon: KeyboardMusic,
+        total: "100",
+        average: 5,
+        title: "Products Created",
+        tooltipText: "See all products created",
+    },
+    {
+        icon: Waypoints,
+        total: "86.5%",
+        average: 80,
+        title: "Total Revenue",
+        tooltipText: "See all of the revenue",
+    },
+    {
+        icon: BookOpenCheck,
+        total: "S/. 265",
+        average: 80,
+        title: "Bounce Rate",
+        tooltipText: "See all of the bounce rate",
+    },
+];
