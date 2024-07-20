cde864a99bba26130189881a24c9742247decb51
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-19 22:51:25 -0500

    feat: Update LastProducts component with custom icon and styling

--- app/(routes)/components/LastProducts/LastProducts.tsx
+++ app/(routes)/components/LastProducts/LastProducts.tsx
@@ -1,5 +1,20 @@
+import { CustomIcon } from "@/components/CustomIcon";
+import { Building } from "lucide-react";
+
 export function LastProducts() {
     return (
-        <div>LastProducts</div>
+        <div className="shadow-sm bg-background rounded-lg p-5">
+            <div className="flex gap-x-2 items-center">
+                <CustomIcon icon={Building} />
+                <p className="text-xl">
+                    Last Products
+                </p>
+            </div>
+            <div>
+                <p>
+                    No products added yet
+                </p>
+            </div>
+        </div>
     )
 }
