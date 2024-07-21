6ae6349072285c9bebbff8ed29daf07d5ad00fa6
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:12:31 -0500

    refactor: Update Navbar component to include language dropdown menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,17 +1,23 @@
-import { Input } from "@/components/ui/input"
+"use client"
+
+import { useLanguage } from "@/contexts/LanguageContext";
+import { Input } from "@/components/ui/input";
 
 import {
     Sheet,
     SheetContent,
     SheetTrigger,
-} from "@/components/ui/sheet"
+} from "@/components/ui/sheet";
 
-import { UserButton } from "@clerk/nextjs"
-import { Menu, Search } from "lucide-react"
-import { SidebarRoutes } from "../SidebarRoutes"
-import { ToggleTheme } from "../ToggleTheme"
+import { UserButton } from "@clerk/nextjs";
+import { Menu, Search, Globe } from "lucide-react";
+import { SidebarRoutes } from "../SidebarRoutes";
+import { ToggleTheme } from "../ToggleTheme";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 
 export function Navbar() {
+    const { setLanguage, translations } = useLanguage();
+
     return (
         <nav className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
 
@@ -29,29 +35,27 @@ export function Navbar() {
 
             {/* Desktop */}
             <div className="relative w-[300px]">
-                <Input placeholder="Search" className="rounded-lg" />
+                <Input placeholder={translations.sidebarRoutes?.Search || 'Search'} className="rounded-lg" />
                 <Search className="absolute top-2 right-2" strokeWidth={1} />
             </div>
 
             <div className="flex gap-x-2 items-center">
-                {/*
                 <DropdownMenu>
                     <DropdownMenuTrigger className="flex items-center">
                         <Globe className="w-5 h-5" />
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuItem onSelect={() => changeLanguage('en')}>
+                        <DropdownMenuItem onSelect={() => setLanguage('en')}>
                             English
                         </DropdownMenuItem>
-                        <DropdownMenuItem onSelect={() => changeLanguage('es')}>
+                        <DropdownMenuItem onSelect={() => setLanguage('es')}>
                             Español
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                */}
                 <ToggleTheme />
                 <UserButton />
             </div>
         </nav>
-    )
+    );
 }
