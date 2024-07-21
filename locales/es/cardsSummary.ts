9dfbed4fc0b29ae5bc1b30b0f8866734a9e642fb
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:13:13 -0500

    refactor: Update Spanish translations for cardsSummary

---
+++ locales/es/cardsSummary.ts
@@ -0,0 +1,10 @@
+const cardsSummary = {
+    "Products Created": "Productos Creados",
+    "Total Revenue": "Ingresos Totales",
+    "Bounce Rate": "Tasa de Rebote",
+    "See all products created": "Ver todos los productos creados",
+    "See all of the revenue": "Ver todos los ingresos",
+    "See all of the bounce rate": "Ver todas las tasas de rebote",
+};
+
+export default cardsSummary;
