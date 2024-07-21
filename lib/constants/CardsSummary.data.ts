2e6a01d032f5f9333c5a98f0e6f05994db8e314b
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:12:52 -0500

    refactor: Update CardsSummary.data.ts to use translations for titles and tooltip texts

--- lib/constants/CardsSummary.data.ts
+++ lib/constants/CardsSummary.data.ts
@@ -1,25 +1,25 @@
 import { BookOpenCheck, KeyboardMusic, Waypoints } from "lucide-react";
 
-export const dataCardsSummary = [
+export const getDataCardsSummary = (translations: { [key: string]: string }) => [
     {
         icon: KeyboardMusic,
         total: "100",
         average: 5,
-        title: "Products Created",
-        tooltipText: "See all products created",
+        title: translations["Products Created"],
+        tooltipText: translations["See all products created"],
     },
     {
         icon: Waypoints,
         total: "86.5%",
         average: 35,
-        title: "Total Revenue",
-        tooltipText: "See all of the revenue",
+        title: translations["Total Revenue"],
+        tooltipText: translations["See all of the revenue"],
     },
     {
         icon: BookOpenCheck,
         total: "S/. 265",
         average: 80,
-        title: "Bounce Rate",
-        tooltipText: "See all of the bounce rate",
+        title: translations["Bounce Rate"],
+        tooltipText: translations["See all of the bounce rate"],
     },
 ];
