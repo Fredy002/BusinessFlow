db9550f63ce0db46f2112044e443eeb767499a67
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:13:07 -0500

    refactor: Update SidebarRoutes to use translations for labels

---
+++ locales/es/sidebarRoutes.ts
@@ -0,0 +1,14 @@
+const sidebarRoutes = {
+    "Dashboard": "Tablero",
+    "Products": "Productos",
+    "Calendar": "Calendario",
+    "Faqs": "Preguntas Frecuentes",
+    "Analytics": "Analíticas",
+    "Settings": "Configuraciones",
+    "Security": "Seguridad",
+    "General": "General",
+    "Tools": "Herramientas",
+    "Support": "Soporte",
+};
+
+export default sidebarRoutes;
