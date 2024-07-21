39321438f9d3398bd565ad32c99cec8c17487b62
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:11:29 -0500

    refactor: Update SidebarRoutes.data.ts to use translation for labels

--- lib/constants/SidebarRoutes.data.ts
+++ lib/constants/SidebarRoutes.data.ts
@@ -8,46 +8,46 @@ import {
     Calendar
 } from "lucide-react";
 
-export const GeneralSidebarData = [
+export const getGeneralSidebarData = (translations: { [key: string]: string }) => [
     {
         icon: PanelsTopLeft,
-        label: 'Dashboard',
+        label: translations['Dashboard'] || 'Dashboard',
         href: '/'
     },
     {
         icon: Building2,
-        label: 'Products',
+        label: translations['Products'] || 'Products',
         href: '/products'
     },
     {
         icon: Calendar,
-        label: 'Calendar',
+        label: translations['Calendar'] || 'Calendar',
         href: '/calendar'
     }
 ];
 
-export const ToolsSidebarData = [
+export const getToolsSidebarData = (translations: { [key: string]: string }) => [
     {
         icon: CircleHelpIcon,
-        label: 'Faqs',
+        label: translations['Faqs'] || 'Faqs',
         href: '/faqs'
     },
     {
         icon: BarChart4,
-        label: 'Analytics',
+        label: translations['Analytics'] || 'Analytics',
         href: '/analytics'
     }
 ];
 
-export const SupportSidebarData = [
+export const getSupportSidebarData = (translations: { [key: string]: string }) => [
     {
         icon: Settings,
-        label: 'Settings',
+        label: translations['Settings'] || 'Settings',
         href: '/settings'
     },
     {
         icon: ShieldCheck,
-        label: 'Security',
+        label: translations['Security'] || 'Security',
         href: '/security'
     }
 ];
