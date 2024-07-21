56f843cb95b5a7db0aa8edb13f12800604be2d59
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:13:50 -0500

    refactor: Update layoutDashboard component to include LanguageProvider

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,18 +1,21 @@
-import { Navbar } from "@/components/Navbar"
-import { Sidebar } from "@/components/Sidebar"
+import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
+import { LanguageProvider } from "@/contexts/LanguageContext";
 
-export default function layoutDashboard({ children }: { children: React.ReactElement }) {
+export default function LayoutDashboard({ children }: { children: React.ReactElement }) {
     return (
-        <div className="flex w-full h-full">
-            <div className="hidden xl:block w-80 h-full xl:fixed">
-                <Sidebar />
-            </div>
-            <div className="w-full xl:ml-80">
-                <Navbar />
-                <div className="p-6 bg-[#fafbfc] dark:bg-secondary">
-                    {children}
+        <LanguageProvider>
+            <div className="flex w-full h-full">
+                <div className="hidden xl:block w-80 h-full xl:fixed">
+                    <Sidebar />
+                </div>
+                <div className="w-full xl:ml-80">
+                    <Navbar />
+                    <div className="p-6 bg-[#fafbfc] dark:bg-secondary">
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        </LanguageProvider>
+    );
 }
