b91a53d47a46d19690048e8eb27676f421add425
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:13:24 -0500

    refactor: Add sidebar routes translations

---
+++ locales/en/sidebarRoutes.ts
@@ -0,0 +1,14 @@
+const sidebarRoutes = {
+    "Dashboard": "Dashboard",
+    "Products": "Products",
+    "Calendar": "Calendar",
+    "Faqs": "Faqs",
+    "Analytics": "Analytics",
+    "Settings": "Settings",
+    "Security": "Security",
+    "General": "General",
+    "Tools": "Tools",
+    "Support": "Support",
+};
+
+export default sidebarRoutes;
