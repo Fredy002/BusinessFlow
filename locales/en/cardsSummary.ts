c3f0e1da63c775391498ebc2d643b183ed5b0b43
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-07-21 15:13:19 -0500

    refactor: Add English translations for cardsSummary

---
+++ locales/en/cardsSummary.ts
@@ -0,0 +1,10 @@
+const cardsSummary = {
+    "Products Created": "Products Created",
+    "Total Revenue": "Total Revenue",
+    "Bounce Rate": "Bounce Rate",
+    "See all products created": "See all products created",
+    "See all of the revenue": "See all of the revenue",
+    "See all of the bounce rate": "See all of the bounce rate",
+};
+
+export default cardsSummary;
